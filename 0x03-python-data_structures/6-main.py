19689a9004f26b5beb0b7b193dd0d55c59246a63
hyperayoub <bouagnaayoub@gmail.com>
2023-10-08 05:43:54 -0700

    py

--- 0x03-python-data_structures/6-main.py
+++ 0x03-python-data_structures/6-main.py
@@ -10,3 +10,4 @@ matrix = [
 print_matrix_integer(matrix)
 print("--")
 print_matrix_integer()
+
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-
-
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
-        for i in range(0, len(matrix)):
-            for j in range(0, len(matrix[i])):
-                if j != len(matrix[i]) - 1:
-                    print("{:d}".format(matrix[i][j]), end=" ")
-                else:
-                    print("{:d}".format(matrix[i][j]))
-    else:
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
         print()
