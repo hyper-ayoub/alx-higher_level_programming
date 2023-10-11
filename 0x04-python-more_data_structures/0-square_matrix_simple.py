f7f9b107511138a97bcfb7063a5f4913e8982f1b
hyperayoub <bouagnaayoub@gmail.com>
2023-10-11 04:21:07 -0700

    task1

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    lenM = len(matrix)
+    new_matrix = [[0 for _ in range(len(matrix[0]))]for _ in range(lenM)]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            new_matrix[i][j] =  matrix[i][j]*matrix[i][j]
+    return new_matrix 
+
