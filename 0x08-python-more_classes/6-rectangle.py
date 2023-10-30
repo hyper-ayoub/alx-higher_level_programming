366b96a9e65a075695fc924f157a53d244047d04
hyperayoub <bouagnaayoub@gmail.com>
2023-10-30 14:08:38 -0700

    update

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -121,4 +121,4 @@ class Rectangle:
         """
         if Rectangle.number_of_instances > 0:
             Rectangle.number_of_instances -= 1
-         print("Bye rectangle...")
+        print("Bye rectangle...")
