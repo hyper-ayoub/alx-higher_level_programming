ca869413654dde2185464ddbfa31e0959224d24a
hyperayoub <bouagnaayoub@gmail.com>
2023-11-07 13:19:35 -0800

    input/output

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-'''To JSON string.'''
+"""'To JSON string."""
 
 
 import json
 
 
 def to_json_string(my_obj):
-    ''' returns the JSON representation of an object string'''
+    """ returns the JSON representation of an object string"""
     return json.dumps(my_obj)
 
