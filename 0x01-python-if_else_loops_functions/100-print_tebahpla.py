4c2a8f7013a118d688c41979d6adc5384679644c
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2023-12-31 23:12:50 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
-# Author -Bamidele Adefolaju
+for i in range(ord('z'), ord('a') - 1, -1):
+    char = chr(i)
+    case = char.upper() if i % 2 == 0 else char.lower()
+    print("{}{}".format(case, char), end='')
 
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
