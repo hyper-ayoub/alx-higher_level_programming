1b83e22d64712faf95443bcf77eb8f952b1a1a31
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2023-12-31 23:49:06 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# Author -Bamidele Adefolaju
-
-i = 0
-for c in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(c - i)), end="")
-    i = 32 if i == 0 else 0
+for i in range(ord('z'), ord('a') - 1, -1):
+    if i % 2 == 0:
+        diff = 0
+    else:
+        diff = 32
+    print('{}'.format(chr(i - diff)), end='')
