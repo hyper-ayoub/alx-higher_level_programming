c0d560165d35ba18328950db258d3dbd89753614
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-01-25 23:35:36 +0100

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Write a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -sX PUT -L -d "user_id=98" --header "origin: X-Yougotme!-User-Id: 98" 0.0.0.0:5000/catch_me
+curl -sX PUT -L -d "user_id=98" --header "origin: HolbertonSchool" 0.0.0.0:5000/catch_me
