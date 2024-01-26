43bb704e39c5d0244b4e9c57b88ebb41b64e2ca4
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-01-26 23:43:10 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Python script that fetches https://intranet.hbtn.io/status"""
+"""Python script that fetches https://alx-intranet.hbtn.io/status """
 
 if __name__ == "__main__":
     import urllib.request
@@ -11,3 +11,4 @@ if __name__ == "__main__":
         print("\t- type:", type(html))
         print("\t- content:", html)
         print("\t- utf8 content:", html.decode('utf-8'))
+
