ba3d5b9decb0eb025c5f1791081b53ac56e38d49
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-01-26 23:24:48 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
    html = response.read() 
-    print("Body response:")
-    print("\t- type:", type(html))
-    print("\t- content:", html)
-    print("\t- utf8 content:", html.decode('utf-8'))
+   print("Body response:")
+   print("\t- type:", type(html))
+   print("\t- content:", html)
+   print("\t- utf8 content:", html.decode('utf-8'))
