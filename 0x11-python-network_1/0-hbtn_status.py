ce39a96fb8021079eebc05b62f8883937a008ac8
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-01-26 23:21:59 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status"""
 
-
     import urllib.request
 
     req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print("Body response:")
         print("\t- type:", type(html))
