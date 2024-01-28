3cc06917516ffe48ee9c599ec6df5c1b1b4cb9f1
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-01-28 17:09:04 +0100

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     param = {"email": email}
-    res = requests.post(url, data = param)
+    res = requests.post(url, data=param)
     print(res.text)
 
