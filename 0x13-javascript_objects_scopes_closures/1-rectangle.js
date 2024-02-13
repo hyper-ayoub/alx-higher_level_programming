900245b4f3dc3dc762833fc48aa9d1f7836f1e47
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-02-13 22:03:04 +0100

    Objects, Scopes and Closures

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+class Rectangle {
+	constructor (w, h) {
+		this.weight = w;
+		this.height = h;
+	}
+}
+
+module.exports = Rectangle;
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,2 @@
+0x13-javascript_objects_scopes_closures
+
