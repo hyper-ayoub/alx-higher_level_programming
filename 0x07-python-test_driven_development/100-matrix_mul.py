4a3455c3b9721934e6f046015691a5954ba27548
ayoub bouagna <133155846+hyper-ayoub@users.noreply.github.com>
2024-03-11 00:28:48 +0100

    Update 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -17,7 +17,7 @@ def matrix_mul(m_a, m_b):
         A new matrix representing the multiplication of m_a by m_b.
     """
 
-    if m_a == [] or m_a == [[]]:
+   if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
@@ -40,9 +40,9 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b should contain only integers or floats")
 
     if not all(len(row) == len(m_a[0]) for row in m_a):
-        raise TypeError("each row of m_a must should be of the same size")
+        raise TypeError("each row of m_a must be of the same size")
     if not all(len(row) == len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_b must should be of the same size")
+        raise TypeError("each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
